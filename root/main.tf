49014b01862842a0a135112a1538b2349b14d7a1
shadyosama9 <shadyosama554@gmail.com>
2024-10-07 13:36:17 +0300

    splunk instance module conf

--- root/main.tf
+++ root/main.tf
@@ -20,3 +20,19 @@ module "ec2-scraper" {
   VOLUME_TYPE = "gp2"
   VOLUME_SIZE = 20
 }
+
+
+module "ec2-splunk" {
+
+  source = "../modules/ec2"
+
+  INSTANCE_TYPE  = "t2.medium"
+  INSTANCE_AMI   = "ami-0866a3c8686eaeeba"
+  INSTANCE_SG_ID = lookup(module.sg.Security-Groups-IDs, "splunk-sg")
+  KEY_NAME       = "Splunk-Key"
+  INSTANCE_TAGS = {
+    "Name" = "Splunk-Server"
+  }
+  VOLUME_TYPE = "gp2"
+  VOLUME_SIZE = 24
+}
